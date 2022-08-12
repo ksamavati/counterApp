9732b971c926aded0fc6fbd538afdc3eea4c8a21
ksamavati <kayvonsamavati@gmail.com>
2022-08-12 12:56:23 -0400

    push 3

--- src/components/SubtractButton.jsx
+++ src/components/SubtractButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const SubtractButton = ({ subtract }) => {
-    return (
-        <button className="btn btn-lg btn-outline-primary" onClick={subtract} id="subButton">-1</button>
-    )
+	return (
+		<button className="btn btn-lg btn-outline-primary" onClick={subtract} id="subButton">-1</button>
+	)
 }
